69e566f966dce468fe656c976ff0bbe74007d744
Beulah Mathenge <beulahmathenge@gmail.com>
2023-02-16 15:53:45 +0300

    adding all files to git

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Gallery from "./Gallery";
+import CardDeal from "./CardDeal";
+import Services from "./Services";
+import Clients from "./Clients";
+import Contact from "./Contact";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Gallery,
+  CardDeal,
+  Services,
+  Clients,
+  Contact,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,103 @@
+
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+  {
+    id: "contact",
+    title: "Contact"
+  }
+];
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "FAQ",
+        link: "/",
+      },
+      {
+        name: "Investor Relations",
+        link: "https://ir.netflix.net/ir-overview/profile/default.aspx",
+      },
+      {
+        name: "Privacy",
+        link: "https://help.netflix.com/legal/privacy",
+      },
+      {
+        name: "Speed Test",
+        link: "https://fast.com/",
+      },
+    ],
+  },
+  {
+    title: "Recommendations",
+    links: [
+
+      {
+        name: "Suggestions",
+        link: "https://www.imdb.com/search/title/?country_of_origin=KE",
+      },
+      {
+        name: "Rotten Tomatoes",
+        link: "https://www.rottentomatoes.com/",
+      },
+      {
+        name: "Kids Shows",
+        link: "https://www.netflix.com/ke/browse/genre/27346",
+      },
+      {
+        name: "Series To Watch",
+        link: "https://collider.com/best-tv-shows-to-binge-watch/",
+      },
+    ],
+  },
+  {
+    title: "Account",
+    links: [
+      {
+        name: "Ways to Watch",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
