a27031033228c6981d305f6768a2d7c664f971a3
Beulah Mathenge <beulahmathenge@gmail.com>
2023-03-04 17:26:44 +0300

    Call to action component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,30 @@
 import styles from '../style'
 import {discount, hero} from '../assets'
+import GetStarted from './GetStarted'
 
 const Hero = () => {
   return (
    <section className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div className={` flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
-      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+      <div id='discount' className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]"/>
         <p className={`${styles.paragraph} ml-2`}>
         <span className='text-white'>10%</span> Discount For {" "}
         <span className='text-white'>New Clients</span>
         </p>
       </div>
+      <div id='description' className='flex flex-row justify-between items-center w-full'>
+        <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]'>
+          Your Reliable, <br className='sm:block hidden' /> {" "}
+          <span className='text-gradient'>Competent</span> {" "}
+        </h1>
+        <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted/>
+          </div>
+      </div>
+      <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">Mechanic Provider.
+      </h1>
+      
     </div> 
    </section>
   )
