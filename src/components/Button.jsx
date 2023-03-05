a52dd39935590239f8cf6a6259948e5aec0bfe37
Beulah Mathenge <beulahmathenge@gmail.com>
2023-03-05 20:45:31 +0300

    ✨ feat: Adding a reusable button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-function Button() {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    Book A Service
+  </button>
+);
 
 export default Button
