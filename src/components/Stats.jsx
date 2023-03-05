468895e09afe319d7334f1e44bc70e03f143ee6c
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2023-03-05 16:23:22 -0500

    added counter stat countup

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,6 @@
 import {stats} from "../constants"
 import styles from "../style"
+import CountUp from "react-countup"
 
 const Stats = () => {
 
@@ -9,7 +10,9 @@ const Stats = () => {
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
       {stats.map((stat) => (
         <div key={stat.id} className={` flex-1 flex justify-center items-center flex-row m-3`}>
-          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">{stat.value} + </h4>
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            <CountUp end={stat.value} enableScrollSpy scrollSpyOnce /> + 
+          </h4>
           <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">{stat.title}</p>
         </div>
       ))}
