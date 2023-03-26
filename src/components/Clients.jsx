b57fa487fa8b4205e3db80b6c66aab43a422417f
Beulah Mathenge <beulahmathenge@gmail.com>
2023-03-26 18:09:57 +0300

    💄 ✨ Finishing up on the UI/UX design of project

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import styles from "../style";
+import { clients } from "../constants";
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {/* find out how you can change the bg color on hover  eg focus-within:hover:bg-slate-300 */}
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}> 
+          <img src={client.logo} alt="client_logo" className="sm:w-[192px] w-[100px] object-contain" />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
 export default Clients
--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,6 +2,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => {
+  // This is the round button on the hero section, it should lead one to contact page.
   return (
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
