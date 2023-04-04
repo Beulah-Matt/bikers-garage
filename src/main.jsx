d555c83ae1fcec89df0a70f95dda149629aa8da5
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-04 13:33:09 +0300

    🎨 Adding Router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErrorPage from './components/ErrorPage';
 import './index.css'
+
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <App />,
+    errorElement: <ErrorPage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
