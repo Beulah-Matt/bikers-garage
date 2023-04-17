3c02c99f62c0eb485babf26b94a753f8d0b0066e
Beulah Mathenge <beulahmathenge@gmail.com>
2023-04-17 22:08:05 +0300

    🚧 Adding routes, work on fixing bug on path does not exist.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styles from "./style";
 import { Gallery, Services, Clients, Contact, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 import Home from './components/Home';
 import CallToAction from './components/CallToAction';
+import { Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -22,13 +23,15 @@ const App = () => {
     <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
-        <Home />
-        <Services />
-        <Gallery />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
         <Testimonials />
         <Clients />
         <CallToAction />
-        <Contact />
         <Footer />
       </div>
     </div>
